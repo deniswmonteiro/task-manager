31ae9c21ae7f434cec5a27dd6db71eb749636fd4
Denis Monteiro <deniswmonteiro@gmail.com>
2026-05-01 18:13:56 -0300

    feat: adicionando transição no AddTaskModal

--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -1,43 +1,70 @@
+import "./AddTaskModal.css";
+
+import React from "react";
 import { createPortal } from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
 import Button from "./Button";
 import Input from "./form/Input";
 
 const AddTaskModal = ({ modalIsOpen, handleModalClose }) => {
-  if (!modalIsOpen) return null;
+  const nodeRef = React.useRef();
 
-  return createPortal(
-    <div className="fixed top-0 left-0 flex h-screen w-screen items-center justify-center backdrop-blur-xs">
-      <div className="w-85 space-y-5 rounded-xl bg-white p-5 text-center shadow">
-        <div>
-          <h2 className="mb-1 text-xl font-semibold text-[#35383e]">
-            Nova tarefa
-          </h2>
-          <p className="text-sm text-[#9a9c9f]">Insira as informações abaixo</p>
-        </div>
-        <div className="space-y-4">
-          <Input id="title" label="Título" placeholder="Título da tarefa" />
-          <Input
-            id="description"
-            label="Descrição"
-            placeholder="Descreva a tarefa"
-          />
-        </div>
-        <div className="flex items-center justify-center gap-3">
-          <Button
-            variant="secondary"
-            size="lg"
-            onClick={() => handleModalClose(false)}
+  // createPortal - é uma forma de renderizar um componente em qualquer lugar do DOM
+  // CSSTransition - é uma forma de adicionar transições CSS ao componente
+  return (
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={modalIsOpen}
+      timeout={300}
+      classNames="add-task-modal"
+      unmountOnExit
+    >
+      <div>
+        {createPortal(
+          <div
+            ref={nodeRef}
+            className="fixed top-0 left-0 flex h-screen w-screen items-center justify-center backdrop-blur-xs"
           >
-            Cancelar
-          </Button>
-          <Button variant="primary" size="lg">
-            Salvar
-          </Button>
-        </div>
+            <div className="w-85 space-y-5 rounded-xl bg-white p-5 text-center shadow">
+              <div>
+                <h2 className="mb-1 text-xl font-semibold text-[#35383e]">
+                  Nova tarefa
+                </h2>
+                <p className="text-sm text-[#9a9c9f]">
+                  Insira as informações abaixo
+                </p>
+              </div>
+              <div className="space-y-4">
+                <Input
+                  id="title"
+                  label="Título"
+                  placeholder="Título da tarefa"
+                />
+                <Input
+                  id="description"
+                  label="Descrição"
+                  placeholder="Descreva a tarefa"
+                />
+              </div>
+              <div className="flex items-center justify-center gap-3">
+                <Button
+                  variant="secondary"
+                  size="lg"
+                  onClick={() => handleModalClose(false)}
+                >
+                  Cancelar
+                </Button>
+                <Button variant="primary" size="lg">
+                  Salvar
+                </Button>
+              </div>
+            </div>
+          </div>,
+          document.body
+        )}
       </div>
-    </div>,
-    document.body
+    </CSSTransition>
   );
 };
 
