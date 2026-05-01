67c48f772bdb516715168396ef9d17b7f06019fb
Denis Monteiro <deniswmonteiro@gmail.com>
2026-05-01 17:53:41 -0300

    feat: adicionando variante de cor e tamanho ao botão

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,37 @@
-const Button = ({ children, variant, ...props }) => {
+const Button = ({ children, variant, size, ...props }) => {
   const getVariantClasses = () => {
     switch (variant) {
       case "primary":
         return "bg-[#00adb5] text-white hover:shadow-sm hover:shadow-[#00adb5]";
 
+      case "secondary":
+        return "bg-[#eeeeee] text-[#35383e] hover:shadow-sm hover:shadow-[#eeeeee]";
+
       case "ghost":
-        return "bg-transparent text-[#818181] hover:text-black";
+        return "bg-transparent text-[#818181] hover:text-[#35383e]";
 
       default:
         return "bg-[#00adb5] text-white hover:shadow-sm hover:shadow-[#00adb5]";
     }
   };
 
+  const getSizeClasses = () => {
+    switch (size) {
+      case "sm":
+        return "px-3  text-xs";
+
+      case "lg":
+        return "px-11 text-sm";
+
+      default:
+        return "px-3 text-xs";
+    }
+  };
+
   return (
     <button
       type="button"
-      className={`flex cursor-pointer items-center gap-1 rounded-md px-3 py-2 text-xs font-semibold duration-300 hover:transition-all hover:duration-300 ${getVariantClasses()}`}
+      className={`flex cursor-pointer items-center gap-1 rounded-md py-2 font-semibold duration-300 hover:transition-all hover:duration-300 ${getVariantClasses()} ${getSizeClasses()}`}
       {...props}
     >
       {children}
